b841bbd36ca13edcbdbce2e8bae0aed8f93704b9
[Hazem Maddouri] <[hazemmaddourib@gmail.com]>
2024-01-16 11:08:52 +0100

    fixed errors

--- register-sw.js
+++ register-sw.js
@@ -1,13 +1,14 @@
-if('serviceWorker' in navigator) {
-  window.addEventListener('load', e => {
-    navigator.serviceWorker.register('sw.js')
-    .then((reg) => {
-      console.log("notify", 'Service worker is started ' + reg.scope)
-    .catch(error => {
-      console.log('alert', 'Service worker registration failed :' + error)
-    })
-    })
-  })
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", (e) => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then((reg) => {
+        console.log("Service worker is registered. Scope: " + reg.scope);
+      })
+      .catch((error) => {
+        console.error("Service worker registration failed: " + error);
+      });
+  });
 } else {
-  console.log("Votre navigateur n'a pas les services Workers")
+  console.log("Votre navigateur n'a pas les services Workers");
 }
